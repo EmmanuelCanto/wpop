9836ae97497ae264c6e96cd9770f5fa9a00be917
Emmanuel Canto <emmanuel-canto@hotmail.com>
2026-03-10 23:39:31 -0500

    Initial Commit

---
+++ FloatingWindow/FloatingWindowApp.swift
@@ -0,0 +1,17 @@
+//
+//  FloatingWindowApp.swift
+//  FloatingWindow
+//
+//  Created by Emmanuel Canto on 10/03/26.
+//
+
+import SwiftUI
+
+@main
+struct FloatingWindowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
